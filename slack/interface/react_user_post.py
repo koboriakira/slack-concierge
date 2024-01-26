7667d2eb5f62d7efb336e3d6bec639cfca7761b9
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-01-26 17:35:31 +0900

    Add logging to react function

--- slack/interface/react_user_post.py
+++ slack/interface/react_user_post.py
@@ -6,14 +6,21 @@ from slack_bolt import App, Ack, Say
 from slack_bolt.context.context import BoltContext
 from util.logging_traceback import logging_traceback
 from domain.message import ExtendedMessage
+from domain.channel import ChannelType
+from util.environment import Environment
 
 def just_ack(ack: Ack):
     ack()
 
-def react(message:dict, say: Say, context: BoltContext):
+def react(message:dict, say: Say, context: BoltContext, logger: logging.Logger):
+    if Environment.is_dev():
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.DEBUG)
+    logger.info("react")
+
     try:
-        logging.info("react")
-        # context.say("Hi!")
+        logger.debug(json.dumps(message, ensure_ascii=False))
         message_model = ExtendedMessage(**message)
         if not message_model.from_kobori():
             return
@@ -22,6 +29,7 @@ def react(message:dict, say: Say, context: BoltContext):
         import sys
         exc_info = sys.exc_info()
         logging_traceback(e, exc_info)
+
 
 def message_react_user_post(app: App):
     app.message(re.compile(".*"))(
