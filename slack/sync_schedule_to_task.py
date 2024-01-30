851a77d51512c329d08c6cfd8417d0c11f7c06aa
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-01-30 23:32:17 +0900

    Add date parameter to sync_schedule_to_task handler

--- slack/sync_schedule_to_task.py
+++ slack/sync_schedule_to_task.py
@@ -1,6 +1,7 @@
 import os
 import json
 import logging
+from datetime import date as Date
 from slack_sdk.web import WebClient
 from domain.schedule.schedule import Schedule
 from infrastructure.api.lambda_google_calendar_api import LambdaGoogleCalendarApi
@@ -24,14 +25,18 @@ def handler(event, context):
     """
     AWS Lambda での実行に対応するハンドラー関数
     """
-    data = google_calendar_api.get_gas_calendar(date=now().date())
+    date = event.get("date")
+    date = Date.fromisoformat(date) if date else None
+    if date is None:
+        raise ValueError("date is required")
+
+    data = google_calendar_api.get_gas_calendar(date=date)
     if data is None:
         return {"message": "no schedule"}
 
     schedules = [Schedule.from_entity(d) for d in data]
     for schedule in schedules:
         create_task(schedule)
-        return
     return {"message": "success"}
 
 def create_task(schedule: Schedule) -> None:
@@ -44,4 +49,7 @@ def create_task(schedule: Schedule) -> None:
 if __name__ == "__main__":
     # python -m sync_schedule_to_task
     logger.debug("debug mode")
-    print(handler({}, {}))
+    event = {
+        "date": "2024-01-31"
+    }
+    print(handler(event, {}))
