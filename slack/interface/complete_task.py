8a9dcf1ddaab72759da5a32ac2cd11a125f6dca1
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-02-02 17:51:56 +0900

    Add SQS integration for task completion

--- slack/interface/complete_task.py
+++ slack/interface/complete_task.py
@@ -1,12 +1,16 @@
 import logging
 import json
+import os
 from slack_sdk.web import WebClient
 from slack_bolt import App, Ack
 from infrastructure.api.lambda_notion_api import LambdaNotionApi
 from usecase.complete_task import CompleteTask as CompleteTaskUsecase
+from usecase.service.sqs_service import SqsService
 from util.logging_traceback import logging_traceback
 
 ACTION_ID = "complete-task"
+AWS_ACCOUNT_ID = os.environ["AWS_ACCOUNT_ID"]
+QUEUE_URL = f"https://sqs.ap-northeast-1.amazonaws.com/{AWS_ACCOUNT_ID}/SlackConcierge-CompleteTaskQueue31F46268-1zWLUVzSO2Bj"
 
 
 def just_ack(ack: Ack):
@@ -23,9 +27,17 @@ def complete_task(body: dict, client: WebClient, logger:logging.Logger):
         # 返信用にchannel_id、thread_tsを取得
         channel_id = body["channel"]["id"]
         thread_ts = body["message"]["ts"]
-        logger.debug(json.dumps(body))
-        usecase = CompleteTaskUsecase(LambdaNotionApi(), client)
-        usecase.handle(notion_page_block_id, channel_id, thread_ts)
+
+        # SQSにメッセージを送信
+        sqs_service = SqsService()
+        sqs_service.send(QUEUE_URL, {
+            "page_id": notion_page_block_id,
+            "channel": channel_id,
+            "thread_ts": thread_ts
+        })
+
+        # タスク完了を返信
+        client.chat_postMessage(text="タスクを完了しました！", channel=channel_id, thread_ts=thread_ts)
     except Exception as err:
         import sys
         logging_traceback(err, sys.exc_info())
--- slack/usecase/complete_task.py
+++ slack/usecase/complete_task.py
@@ -1,14 +1,58 @@
+from enum import Enum
+from datetime import datetime as Datetime
+from datetime import timedelta
+from typing import Optional
 from slack_sdk.web import WebClient
 from domain.infrastructure.api.notion_api import NotionApi
+from domain.notion.notion_page import TaskPage
+from usecase.service.event_bridge_scheduler_service import EventBridgeSchedulerService
+from util.datetime import now as datetime_now
 
+
+class RoutineTask(Enum):
+    """ ルーティンタスク """
+    CLEANING_BATHROOM = "風呂掃除"
+
+    @property
+    def datetime_creates_next_task(self) -> Datetime:
+        """ 次回のタスクの起票予約日時。00:00は日付のみの指定とする """
+        now = datetime_now()
+        match self:
+            case RoutineTask.CLEANING_BATHROOM:
+                target_datetime = now + timedelta(days=7)
+                return target_datetime.replace(hour=0, minute=0, second=0, microsecond=0)
+            case _:
+                raise NotImplementedError(f"未実装のルーティンタスクです: {self.value}")
+
+    @staticmethod
+    def from_name(name: str) -> 'RoutineTask':
+        return RoutineTask[name.replace("【ルーティン】", "")]
 
 class CompleteTask:
     def __init__(self, notion_api: NotionApi, client: WebClient):
         self.notion_api = notion_api
         self.client = client
+        self.scheduler_client = EventBridgeSchedulerService()
 
     def handle(self, block_id: str, channel: str, thread_ts: str):
         """ タスクを完了する """
         # ステータスを更新
         self.notion_api.update_status(page_id=block_id, value="Done")
-        self.client.chat_postMessage(text="タスクを完了しました！", channel=channel, thread_ts=thread_ts)
+
+        # ルーティンタスクであれば、次回のタスクの起票予約を行う
+        task:TaskPage = self.notion_api.find_task(page_id=block_id)
+        if task.is_routine:
+            self._reserve_next_task(task_title=task.title)
+
+    def _reserve_next_task(self, task_title: str):
+        """ 次回のタスクの起票予約を行う """
+        routine_task = RoutineTask[task_title]
+        self.scheduler_client.set_create_task(
+            task_title=task_title,
+            future_datetime=routine_task.datetime_creates_next_task)
+
+if __name__ == "__main__":
+    # python -m usecase.complete_task
+    import os
+    usecase = CompleteTask(notion_api=NotionApi(), client=WebClient(token=os.environ["SLACK_BOT_TOKEN"]))
+    usecase._reserve_next_task(task_title="【ルーティン】風呂掃除")
