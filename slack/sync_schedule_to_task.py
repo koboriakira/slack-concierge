b9f5a21df08ed12b5c407f88460b629e369b570b
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-02-02 10:22:46 +0900

    Add video recommendation during break time

--- slack/sync_schedule_to_task.py
+++ slack/sync_schedule_to_task.py
@@ -50,6 +50,6 @@ if __name__ == "__main__":
     # python -m sync_schedule_to_task
     logger.debug("debug mode")
     event = {
-        "date": "2024-01-31"
+        "date": "2024-02-02"
     }
     print(handler(event, {}))
--- slack/usecase/pomodoro_timer.py
+++ slack/usecase/pomodoro_timer.py
@@ -1,6 +1,8 @@
+import random
 from slack_sdk.web import WebClient
 from domain_service.block.block_builder import BlockBuilder
 from domain.user import UserKind
+from util.datetime import now
 
 
 class PomodoroTimer:
@@ -14,6 +16,10 @@ class PomodoroTimer:
         block_builder = BlockBuilder()
         block_builder = block_builder.add_section(
             text=f"{user_mention}\n25分が経過しました！\n進捗や気持ちをメモして休憩してください。"
+        )
+        video_5minutes = _select_video()
+        block_builder = block_builder.add_section(
+            text=f"休憩時にオススメ！\n{video_5minutes}"
         )
         block_builder = block_builder.add_button_action(
             action_id="start-pomodoro",
@@ -34,6 +40,27 @@ class PomodoroTimer:
         blocks = block_builder.build()
 
         self.client.chat_postMessage(text="", blocks=blocks, channel=channel, thread_ts=thread_ts)
+
+def _select_video() -> str:
+    hour = now().time().hour
+    if hour < 12:
+        # 朝活、ヨガ系の動画
+        urls = [
+            "https://youtube.com/watch?v=8FX9ZwDvf_0&si=Bw3P_R-ki7I3RnUo"
+        ]
+        return random.choice(urls)
+    elif 12 <= hour < 18:
+        # 筋トレなどの動画
+        urls = [
+            "https://youtube.com/watch?v=8FX9ZwDvf_0&si=Bw3P_R-ki7I3RnUo" # あとで差し替える
+        ]
+        return random.choice(urls)
+    else:
+        # リラックス系の動画
+        urls = [
+            "https://youtube.com/watch?v=8FX9ZwDvf_0&si=Bw3P_R-ki7I3RnUo" # あとで差し替える
+        ]
+        return random.choice(urls)
 
 if __name__ == "__main__":
     # python -m usecase.pomodoro_timer
