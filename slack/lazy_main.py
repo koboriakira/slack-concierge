d32626d8abc62fb75a6ec014705b672d88dcb19c
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-03-06 11:02:34 +0900

    Slackハンドラーにレイジーモードを追加

--- slack/lazy_main.py
+++ slack/lazy_main.py
@@ -3,16 +3,23 @@ from lazy_app import create_lazy_app
 
 app = create_lazy_app()
 
+
 def handler(event, context):
     """
     AWS Lambda での実行に対応するためのハンドラー関数
     """
+    headers: dict = event.get("headers", {})
+    if headers.get("x-slack-bolt-lazy-only") is not None:
+        # レイジーモードの場合は、ハンドラーを呼び出さない
+        # この分岐を入れないと、一度のリクエストで複数回のレスポンスが返されてしまう
+        return {}
     from slack_bolt.adapter.aws_lambda import SlackRequestHandler
 
     SlackRequestHandler.clear_all_log_handlers()
     print("handler is called")
     slack_handler = SlackRequestHandler(app=app)
     return slack_handler.handle(event, context)
+
 
 if __name__ == "__main__":
     # ローカルでの実行に対応するため
