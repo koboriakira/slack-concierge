9c09e91e710c962af2e308e0f1bf39032d5b55aa
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-03-11 00:40:35 +0900

    Add IS_DEMO flag to .env.template and implement is_demo() method in Environment class

--- slack/util/environment.py
+++ slack/util/environment.py
@@ -1,6 +1,11 @@
 import os
 
+
 class Environment:
     @staticmethod
-    def is_dev():
+    def is_dev() -> bool:
         return os.environ.get("ENVIRONMENT") == "dev"
+
+    @staticmethod
+    def is_demo() -> bool:
+        return os.environ.get("IS_DEMO") == "true"
