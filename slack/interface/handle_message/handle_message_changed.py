27078021e3c2d50d4d1fa96fc645915c9df08bb6
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-03-12 11:15:29 +0900

    slack_threadを付与してリクエストする

--- slack/interface/handle_message/handle_message_changed.py
+++ slack/interface/handle_message/handle_message_changed.py
@@ -3,6 +3,7 @@ from logging import Logger
 from slack_sdk.web import WebClient
 
 from domain.channel import ChannelType
+from domain.channel.thread import Thread
 from domain.user import UserKind
 from infrastructure.slack.slack_client_wrapper import SlackClientWrapper
 from usecase.analyze_webpage_use_case import AnalyzeWebpageUseCase
@@ -13,18 +14,19 @@ def handle_message_changed(event: dict, logger: Logger, client: WebClient) -> No
     try:
         channel:str = event["channel"]
         message: dict = event["message"]
+        thread_ts = message["ts"]
         if _is_posted_link_in_inbox_channel(channel, message):
             logger.info("inboxチャンネルへのリンク投稿")
             attachment = message["attachments"][0]
-            channel = event["channel"]
-            thread_ts = event["message"]["ts"]
+            original_url=attachment["original_url"]
             client_wrapper = SlackClientWrapper(client=client, logger=logger)
             if client_wrapper.is_reacted(name="white_check_mark", channel=channel, timestamp=thread_ts):
                 logger.info("既にリアクションがついているので処理をスキップします。")
                 return
             client_wrapper.reactions_add(name="white_check_mark", channel=channel, timestamp=thread_ts)
             usecase = AnalyzeWebpageUseCase(logger=logger)
-            usecase.handle(original_url=attachment["original_url"], attachment=attachment)
+            slack_thread = Thread(channel_id=channel, thread_ts=thread_ts)
+            usecase.handle(original_url=original_url, attachment=attachment, slack_thread=slack_thread)
     except Exception:
         import sys
         import traceback
