2d3e344e1135e9a4a508c89124cddc65359165e2
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-03-12 11:21:56 +0900

    チャンネルタイプをテストからダイアリーに変更

--- test/usecase/start_task_use_case_test.py
+++ test/usecase/start_task_use_case_test.py
@@ -58,7 +58,7 @@ class StartTaskUseCaseTest(TestCase):
         self.suite.scheduler_service.set_pomodoro_timer.assert_called_once_with(
             request=PomodoroTimerRequest(
                 page_id=task.task_id,
-                channel=ChannelType.TEST.value,
+                channel=ChannelType.DIARY.value,
                 thread_ts=DEMO_TS,
             ),
         )
@@ -81,7 +81,7 @@ class StartTaskUseCaseTest(TestCase):
         self.suite.scheduler_service.set_pomodoro_timer.assert_called_once_with(
             request=PomodoroTimerRequest(
                 page_id=task.task_id,
-                channel=ChannelType.TEST.value,
+                channel=ChannelType.DIARY.value,
                 thread_ts=DEMO_TS,
             ),
         )
