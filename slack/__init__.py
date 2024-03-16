c58bfc641898bffa4e4c420bfe65530c571cee2b
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-03-16 22:34:11 +0900

    ロギングの設定を追加***

--- slack/__init__.py
+++ slack/__init__.py
@@ -1,5 +1,13 @@
+import logging
+import os
 import pathlib
 import sys
 
 # このファイルがあるディレクトリをパスに追加
 sys.path.append(str(pathlib.Path(__file__).resolve().parent))
+
+# ロギングの設定
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+if os.environ.get("ENVIRONMENT") == "dev":
+    logger.setLevel(logging.DEBUG)
