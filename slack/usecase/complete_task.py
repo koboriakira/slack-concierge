21e96c3986a75d85324a45e4a2d005fbd20f322a
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-03-18 23:24:46 +0900

    フォーマット

--- slack/usecase/complete_task.py
+++ slack/usecase/complete_task.py
@@ -1,19 +1,18 @@
 from slack_sdk.web import WebClient
+
 from domain.infrastructure.api.notion_api import NotionApi
 from domain.notion.notion_page import TaskPage
-from usecase.service.event_bridge_scheduler_service import EventBridgeSchedulerService
-from util.datetime import now as datetime_now
 from domain.routine.routine_task import RoutineTask
-
+from usecase.service.event_bridge_scheduler_service import EventBridgeSchedulerService
 
 
 class CompleteTask:
-    def __init__(self, notion_api: NotionApi, client: WebClient):
+    def __init__(self, notion_api: NotionApi, client: WebClient) -> None:
         self.notion_api = notion_api
         self.client = client
         self.scheduler_client = EventBridgeSchedulerService()
 
-    def handle(self, block_id: str):
+    def handle(self, block_id: str) -> None:
         """ タスクを完了する """
         # ステータスを更新
         self.notion_api.update_status(page_id=block_id, value="Done")
@@ -23,16 +22,9 @@ class CompleteTask:
         if task.is_routine:
             self._reserve_next_task(task_title=task.title)
 
-    def _reserve_next_task(self, task_title: str):
+    def _reserve_next_task(self, task_title: str) -> None:
         """ 次回のタスクの起票予約を行う """
         routine_task = RoutineTask.from_name(name=task_title)
         self.scheduler_client.set_create_task(
             title=task_title,
             datetime=routine_task.datetime_creates_next_task)
-
-if __name__ == "__main__":
-    # python -m usecase.complete_task
-    from infrastructure.api.lambda_notion_api import LambdaNotionApi
-    import os
-    usecase = CompleteTask(notion_api=LambdaNotionApi(), client=WebClient(token=os.environ["SLACK_BOT_TOKEN"]))
-    usecase.handle(block_id="86eafe45018d418eb83fcbef4e5fcea3")
