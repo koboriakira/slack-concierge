e23060b4e86da20fa8aa75eadf30594f00f64e0a
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-03-21 01:37:33 +0900

    slack/lazy_app.pyの変更をコミット

--- slack/lazy_app.py
+++ slack/lazy_app.py
@@ -3,10 +3,8 @@ import os
 from slack_bolt import App
 
 from interface.complete_task import action_complete_task
-from interface.create_calendar import shortcut_create_calendar
 from interface.handle_message_event import handle_message_event
 from interface.love_spotify_track import shortcut_love_spotify_track
-from interface.react_user_post import message_react_user_post
 from interface.regist import shortcut_regist
 from interface.start_pomodoro import action_start_pomodoro
 from interface.start_task import shortcut_start_task
@@ -21,7 +19,6 @@ def create_lazy_app() -> App:
     )
 
     # 以下、扱える処理をusecase単位で追加
-    app = shortcut_create_calendar(app)
     app = shortcut_sync_times(app)
     app = shortcut_love_spotify_track(app)
     app = shortcut_regist(app)
@@ -29,6 +26,5 @@ def create_lazy_app() -> App:
     app = action_start_pomodoro(app)
     app = action_complete_task(app)
     app = handle_message_event(app)
-    app = message_react_user_post(app)
 
     return app  # noqa: RET504
