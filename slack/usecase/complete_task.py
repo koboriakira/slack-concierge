44df0f0f86e762f3f63de87ff055468d0acc2795
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-03-20 16:53:09 +0900

    ルーティンタスクの起票予約を削除する

--- slack/usecase/complete_task.py
+++ slack/usecase/complete_task.py
@@ -1,5 +1,4 @@
 
-from domain.routine.routine_task import RoutineTask
 from domain.task.task_repository import TaskRepository
 from usecase.service.event_bridge_scheduler_service import EventBridgeSchedulerService
 
@@ -19,17 +18,7 @@ class CompleteTask:
         task.complete()
         self.task_repository.save(task=task)
 
-        # ルーティンタスクであれば、次回のタスクの起票予約を行う
-        if task.is_routine:
-            # FIXME: タスクのタイトルだけなく、インスタンスごと渡したい
-            self._tmp_reserve_next_task(task_title=task.title)
 
-    def _tmp_reserve_next_task(self, task_title: str) -> None:
-        """ 次回のタスクの起票予約を行う """
-        routine_task = RoutineTask.from_name(name=task_title)
-        self.scheduler_client.set_create_task(
-            title=task_title,
-            datetime=routine_task.datetime_creates_next_task)
 
 if __name__ == "__main__":
     from infrastructure.task.notion_task_repository import NotionTaskRepository
