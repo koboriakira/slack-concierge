b5e7566238c641f16aa6c862ccdf0fa8585f476c
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-03-21 09:48:19 +0900

    datetimeモジュールのimportを修正し、Taskクラスを使用するように変更***

--- slack/notificate_schedule.py
+++ slack/notificate_schedule.py
@@ -1,12 +1,11 @@
-from datetime import timedelta
+from datetime import datetime, timedelta
 
 from slack_sdk.web import WebClient
 
 from domain.channel import ChannelType
 from domain.channel.thread import Thread
-from domain.notion.notion_page import TaskPage
+from domain.task.task import Task
 from domain.task.task_button_service import TaskButtonSerivce
-from infrastructure.api.lambda_notion_api import LambdaNotionApi
 from infrastructure.schedule.achievement_repository_impl import AchivementRepositoryImpl
 from infrastructure.task.notion_task_repository import NotionTaskRepository
 from usecase.service.event_bridge_scheduler_service import EventBridgeSchedulerService
@@ -35,19 +34,24 @@ def handler(event:dict, context:dict) -> dict:  # noqa: ARG001
         ErrorReporter().execute()
         return {"message": "error"}
 
-def _find_task() -> list:
+def _find_task() -> list[Task]:
+    from infrastructure.task.notion_task_repository import NotionTaskRepository
+    task_repository = NotionTaskRepository()
+    tasks = task_repository.fetch_current_tasks()
     now = _now()
     # now = Datetime(year=now.year, month=now.month, day=now.day, hour=11, minute=0)
     after_5minutes = now + timedelta(minutes=5)
-    # FIXME: リファクタする
-    tasks = LambdaNotionApi().list_tasks(start_date=now.date())
-    return [task for task in tasks if task.start_date is not None and now.timestamp() <= task.start_date.timestamp() <= after_5minutes.timestamp()]
+    return [task for task in tasks if is_valid(task, now, after_5minutes)]
 
+def is_valid(task: Task, started_at: datetime, end_at: datetime) -> bool:
+    if task.start_date is None:
+        return False
+    return started_at.timestamp() <= task.start_date.timestamp() <= end_at.timestamp()
 
-def post_task(task: TaskPage) -> None:
+def post_task(task: Task) -> None:
     channel = ChannelType.DIARY if not Environment.is_dev() else ChannelType.TEST
     thread = Thread.create(channel_id=channel.value)
-    usecase.handle(task_page_id=task.id, slack_thread=thread)
+    usecase.handle(task_page_id=task.task_id, slack_thread=thread)
 
 
 if __name__ == "__main__":
