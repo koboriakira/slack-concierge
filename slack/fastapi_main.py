b304cb3b8ebc2fd7a540fd58a26c02897194c320
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-04-25 18:09:51 +0900

    POSTのbodyにする

--- slack/fastapi_main.py
+++ slack/fastapi_main.py
@@ -25,7 +25,7 @@ from util.error_reporter import ErrorReporter
 
 # ログ
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG) # 一時的にデバッグレベル高くする
+logger.setLevel(logging.DEBUG)  # 一時的にデバッグレベル高くする
 if Environment.is_dev():
     logger.setLevel(logging.DEBUG)
 
@@ -41,19 +41,26 @@ app = FastAPI(
     version="0.0.1",
 )
 
+
 @app.get("/healthcheck")
 def healthcheck() -> dict:
     return {"status": "ok"}
 
+
+class PostNewTaskRequest(BaseModel):
+    task_name: str
+
+
 @app.post("/task/new/")
-def post_new_task(task_name: str) -> dict:
+def post_new_task(request: PostNewTaskRequest) -> dict:
     try:
         start_task_use_case = StartTaskUseCase()
-        response = start_task_use_case.execute(task_id=None, task_title=task_name)
+        response = start_task_use_case.execute(task_id=None, task_title=request.task_name)
         return {"id": response.task_id, "title": response.title}
     except:  # noqa: E722
         ErrorReporter().execute()
         return {"status": "error"}
+
 
 @app.post("/task/start/{task_id}")
 def post_start_task(task_id: str) -> dict:
@@ -65,15 +72,13 @@ def post_start_task(task_id: str) -> dict:
         ErrorReporter().execute()
         return {"status": "error"}
 
+
 class PageAddContextRequest(BaseModel):
     data: dict
 
 
 @app.post("/message/{channel}/{event_ts}/block/add_context")
-def post_add_context(
-    channel: str,
-    event_ts: str,
-    request: PageAddContextRequest) -> dict:
+def post_add_context(channel: str, event_ts: str, request: PageAddContextRequest) -> dict:
     try:
         usecase = AppendContextUseCase()
         usecase.execute(channel=channel, event_ts=event_ts, data=request.data)
@@ -82,67 +87,65 @@ def post_add_context(
         ErrorReporter().execute()
         return {"status": "error"}
 
+
 @app.post("/wakeup")
 def post_wakeup() -> dict:
     try:
         # 実績を記録
         achivement_repository = AchievementRepositoryImpl(google_cal_api=google_calendar_api)
-        usecase = WakeUpUseCase(
-            achievement_repository=achivement_repository,
-            logger=logger)
+        usecase = WakeUpUseCase(achievement_repository=achivement_repository, logger=logger)
         usecase.execute()
 
         # タスクをリストアップ
         # FIXME: UseCaseはひとつにまとめる
-        slack_client=WebClient(token=os.environ["SLACK_BOT_TOKEN"])
+        slack_client = WebClient(token=os.environ["SLACK_BOT_TOKEN"])
         task_button_service = TaskButtonSerivce(slack_client=slack_client)
         use_case = ListTasksUseCase(
             slack_client=slack_client,
             task_repository=NotionTaskRepository(),
-            task_button_service=task_button_service)
+            task_button_service=task_button_service,
+        )
         use_case.execute(target_date=jst_now().date())
     except:  # noqa: E722
         ErrorReporter().execute()
         return {"status": "error"}
 
+
 @app.post("/sleep")
 def post_sleep() -> dict:
     try:
         achivement_repository = AchievementRepositoryImpl(google_cal_api=google_calendar_api)
-        usecase = SleepUseCase(
-            achievement_repository=achivement_repository,
-            logger=logger)
+        usecase = SleepUseCase(achievement_repository=achivement_repository, logger=logger)
         usecase.execute()
         return {"status": "ok"}
     except:  # noqa: E722
         ErrorReporter().execute()
         return {"status": "error"}
+
 
 @app.post("/goout")
 def post_goout() -> dict:
     try:
         achivement_repository = AchievementRepositoryImpl(google_cal_api=google_calendar_api)
-        usecase = GoOutUseCase(
-            achievement_repository=achivement_repository,
-            logger=logger)
+        usecase = GoOutUseCase(achievement_repository=achivement_repository, logger=logger)
         usecase.execute()
         return {"status": "ok"}
     except:  # noqa: E722
         ErrorReporter().execute()
         return {"status": "error"}
 
+
 @app.post("/come_home")
 def post_come_home() -> dict:
     try:
         achivement_repository = AchievementRepositoryImpl(google_cal_api=google_calendar_api)
-        usecase = ComeHomeUseCase(
-            achievement_repository=achivement_repository,
-            logger=logger)
+        usecase = ComeHomeUseCase(achievement_repository=achivement_repository, logger=logger)
         usecase.execute()
         return {"status": "ok"}
     except:  # noqa: E722
         ErrorReporter().execute()
         return {"status": "error"}
+
 
 app.include_router(post.router, prefix="/post", tags=["post"])
 
