6561102272e61f4d7378051a6181e806ef681d69
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-05-09 15:53:35 +0900

    スケジュール通知の改善

--- slack/notificate_schedule.py
+++ slack/notificate_schedule.py
@@ -1,60 +1,68 @@
-from datetime import date, datetime, timedelta
+from datetime import datetime, timedelta
+from logging import getLogger
 
 from slack_sdk.web import WebClient
 
-from domain.channel import ChannelType
-from domain.channel.thread import Thread
 from domain.task.task import Task
-from domain.task.task_button_service import TaskButtonSerivce
-from infrastructure.schedule.achievement_repository_impl import AchievementRepositoryImpl
+from infrastructure.api.lambda_google_calendar_api import LambdaGoogleCalendarApi
 from infrastructure.task.notion_task_repository import NotionTaskRepository
 from usecase.service.event_bridge_scheduler_service import EventBridgeSchedulerService
-from usecase.start_pomodoro import StartPomodoro as StartPomodoroUsecase
+from usecase.start_task_use_case import StartTaskUseCase
 from util.datetime import now as _now
 from util.environment import Environment
 from util.error_reporter import ErrorReporter
 
+logger = getLogger(__name__)
+logger.setLevel("DEBUG")
+
 client = WebClient(token=Environment.get_slack_bot_token())
-usecase = StartPomodoroUsecase(
-    task_button_service=TaskButtonSerivce(slack_client=client),
-    achievement_repository=AchievementRepositoryImpl(),
+usecase = StartTaskUseCase(
     task_repository=NotionTaskRepository(),
+    google_cal_api=LambdaGoogleCalendarApi(),
+    client=client,
     scheduler_service=EventBridgeSchedulerService(slack_client=client),
+    logger=logger,
 )
 
-def handler(event:dict, context:dict) -> dict:  # noqa: ARG001
+
+def handler(event: dict, context: dict) -> dict:  # noqa: ARG001
     try:
         now_tasks = _find_task()
         if len(now_tasks) == 0:
             return {"message": "no task"}
         for task in now_tasks:
-            post_task(task)
+            usecase.execute(task_id=task.task_id)
         return {"message": "success"}
     except:  # noqa: E722
         ErrorReporter().execute()
         return {"message": "error"}
 
+
 def _find_task() -> list[Task]:
     from infrastructure.task.notion_task_repository import NotionTaskRepository
+
     task_repository = NotionTaskRepository()
     tasks = task_repository.fetch_current_tasks()
     now = _now()
-    # now = Datetime(year=now.year, month=now.month, day=now.day, hour=11, minute=0)
+    # now = datetime(
+    #     year=now.year,
+    #     month=now.month,
+    #     day=now.day,
+    #     hour=9,
+    #     minute=55,
+    #     tzinfo=now.tzinfo,
+    # )
     after_5minutes = now + timedelta(minutes=5)
     return [task for task in tasks if is_valid(task, now, after_5minutes)]
+
 
 def is_valid(task: Task, started_at: datetime, end_at: datetime) -> bool:
     if task.start_date is None:
         return False
     # 時刻がない場合も無視
-    if isinstance(task.start_date, date):
+    if not isinstance(task.start_date, datetime):
         return False
-    return started_at.timestamp() <= task.start_date.timestamp() <= end_at.timestamp()
-
-def post_task(task: Task) -> None:
-    channel = ChannelType.DIARY if not Environment.is_dev() else ChannelType.TEST
-    thread = Thread.create(channel_id=channel.value)
-    usecase.handle(task_page_id=task.task_id, slack_thread=thread)
+    return started_at.timestamp() <= task.start_date.timestamp() and task.start_date.timestamp() <= end_at.timestamp()
 
 
 if __name__ == "__main__":
