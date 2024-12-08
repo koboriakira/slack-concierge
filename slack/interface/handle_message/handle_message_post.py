217f2806e6e1aa38d1611c05ce450b84ed2f0f16
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-12-08 21:41:33 +0900

    feat: add food and drink creation functionality in Slack integration

--- slack/interface/handle_message/handle_message_post.py
+++ slack/interface/handle_message/handle_message_post.py
@@ -1,32 +1,61 @@
 import logging
 
+from domain.channel import ChannelType
+from infrastructure.api.lambda_notion_api import LambdaNotionApi
+from infrastructure.task.notion_task_repository import NotionTaskRepository
 from slack_sdk.errors import SlackApiError
 from slack_sdk.web import WebClient
-
-from domain.channel import ChannelType
-from infrastructure.task.notion_task_repository import NotionTaskRepository
+from usecase.create_food_drink_use_case import CreateFoodDrinkUseCase
 from usecase.create_task_in_inbox import CreateTaskInInbox
 from util.slack_client_wrapper import SlackClientWrapperImpl
 
 
 def handle_message_post(event: dict, logger: logging.Logger, client: WebClient):
     logger.info("スレッド開始")
-    channel:str = event["channel"]
-    user:str = event["user"]
-    event_ts:str = event["ts"]
-    text:str = event["text"]
+    channel: str = event["channel"]
+    user: str = event["user"]
+    event_ts: str = event["ts"]
+    text: str = event["text"]
     blocks: list[dict] = event["blocks"]
 
     if _is_inbox_post(channel=channel, blocks=blocks):
         client_wrapper = SlackClientWrapperImpl(client=client, logger=logger)
         try:
-            client_wrapper.reactions_add(name="inbox_tray", channel=channel, timestamp=event_ts, exception_enabled=True)
+            client_wrapper.reactions_add(
+                name="inbox_tray",
+                channel=channel,
+                timestamp=event_ts,
+                exception_enabled=True,
+            )
             usecase = CreateTaskInInbox(task_repository=NotionTaskRepository())
             usecase.handle(text=text, event_ts=event_ts, channel=channel)
             return
         except SlackApiError as e:
             logger.error(f"Slack APIエラー: {e}")
             return
+
+    if _is_food_drink_post(channel=channel):
+        client_wrapper = SlackClientWrapperImpl(client=client, logger=logger)
+        try:
+            client_wrapper.reactions_add(
+                name="inbox_tray",
+                channel=channel,
+                timestamp=event_ts,
+                exception_enabled=True,
+            )
+            usecase = CreateFoodDrinkUseCase(noton_api=LambdaNotionApi())
+            usecase.handle(text=text, event_ts=event_ts, channel=channel)
+            return
+        except SlackApiError as e:
+            logger.error(f"Slack APIエラー2: {e}")
+            return
+
+
+def _is_food_drink_post(channel: str) -> bool:
+    """
+    INBOXチャンネルへの投稿かどうか判断する
+    """
+    return channel == ChannelType.INBOX.value
 
 
 def _is_inbox_post(channel: str, blocks: list[dict]) -> bool:
---
+++ slack/usecase/create_food_drink_use_case.py
@@ -0,0 +1,19 @@
+from domain.infrastructure.api.notion_api import NotionApi
+from usecase.service.slack_user_client import SlackUserClient
+
+
+class CreateFoodDrinkUseCase:
+    def __init__(self, noton_api: NotionApi) -> None:
+        self._notion_api = noton_api
+        self.user_client = SlackUserClient()
+
+    def handle(self, text: str, event_ts: str, channel: str) -> None:
+        """飲食ページを作成する"""
+        title = text if "\n" not in text else text.split("\n")[0]
+        data = {
+            "title": title,
+        }
+        response = self._notion_api.post(path="/food_drink", data=data)
+        self.user_client.handle(
+            page_id=response["id"], channel=channel, thread_ts=event_ts
+        )
